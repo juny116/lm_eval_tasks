c66a9544e08fa8eee114c2072d188c2b85758f79
juny116 <nsml@node0.run-41isv4xx8cr09sj1vmx0.nsml-nodes.svc.cluster.local>
2026-04-09 02:27:32 +0000

    Remove .ipynb_checkpoints from tracking and add to .gitignore

--- kmmlu_cot/.ipynb_checkpoints/preprocess-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def labeler(doc):
-    pass
-    # answer_list = ["(A)", "(B)", "(C)", "(D)", "(E)"]
-    # return answer_list.index(doc["answer"])
-
-def choicer(doc):
-    cand_list = [ doc['A'], doc['B'], doc['C'], doc['D'] ]
-    return cand_list
